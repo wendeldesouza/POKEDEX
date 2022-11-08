96049df77f3da76c3f5ddbea1f0e885100918f64
wendell <rolim_sk8@hotmail.com>
2022-11-08 00:47:57 -0300

    Pokedex
    nova atualização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
@@ -23,15 +18,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-.then((Response) => Response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        
-    }
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 })
-
-.catch((error) => console.error(error))
